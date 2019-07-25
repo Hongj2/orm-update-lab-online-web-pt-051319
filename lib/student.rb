c2e16c132d5060442c059fa5522e0be31edba0fa
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-25 22:54:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ class Student
     WHERE students.name = ?
     LIMIT 1
     SQL
-      DB[:conn].execute(sql,name).map do |row|
+      DB[:conn].execute(sql, name).map do |row|
         self.new_from_db(row)
       end
     end
